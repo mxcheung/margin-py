f5653415f0c922f041b706841212df7cdae15575
cheungm <maxcheung@optusnet.com.au>
2023-04-29 17:04:27 +1000

    fix unit test

--- src/tests/margin/test_margin_mapper.py
+++ src/tests/margin/test_margin_mapper.py
@@ -11,7 +11,7 @@ class TestMarginMapper(unittest.TestCase):
     def test_map_row(self):
         row = self.input_row()
         actual = self.obj.map_row(self,row)
-        self.assertEqual(1.25, actual['marginAmount'])  # add assertion here
+        self.assertEqual(1.23, actual['marginAmount'])  # add assertion here
 
     def input_row(self):
         return {'Product Code': 'BHP'}
